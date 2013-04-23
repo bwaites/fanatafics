edab04d1f0f63fa1076cc0d9af484a2927f2e192
bwaites <bwaites30284@students.iticollege.edu>
2013-04-23 16:51:11 -0500

    new commit from school

---
+++ FanataficsFinal/Site/AddChapter.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="AddChapter.aspx.cs" Inherits="Site.AddChapter" %>
+<%@ Register Assembly="CKEditor.NET" Namespace="CKEditor.NET" TagPrefix="CKEditor" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+    
+<%--    <link href="Assets/css/wysiwyg-color.css" rel="stylesheet" type="text/css" />
+
+
+    <script src="Assets/js/prettify.js" type="text/javascript"></script>
+    
+    <%--<script src="Assets/js/wysihtml5-0.3.0.min.js" type="text/javascript"></script>--%>
+    <script type="text/javascript" src="Assets/js/jquery-1.7.2.min.js"></script>
+    <script src="Assets/js/nicEdit.js" type="text/javascript"></script>
+
+    </asp:Content>
+
+    
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+<div class="form-horizontal">
+        <!-- Form Name -->
+        <h2>
+            Add Chapter</h2>
+        <!-- Select Basic -->
+        <div class="control-group">
+            <label class="control-label">
+                Choose Story</label>
+            <div class="controls">
+                <select id="sbStory" name="sbStory" class="input-xlarge">
+                </select>
+            </div>
+        </div>
+        <!-- Text input-->
+        <div class="control-group">
+            <label class="control-label">
+                Chapter Title</label>
+            <div class="controls">
+                <input id="txtChapTitle" name="txtChapTitle" type="text" placeholder="" class="input-xlarge"
+                    required="">
+                <p class="help-block">
+                </p>
+            </div>
+        </div>
+        <!-- Select Basic -->
+        <div class="control-group">
+            <label class="control-label">
+                Placement</label>
+            <div class="controls">
+                <select id="sbChapPlace" name="sbChapPlace" class="input-xlarge">
+                </select>
+            </div>
+        </div>
+
+        <textarea class="textarea" id="txtarea" style="width: 810px; height: 200px;" placeholder="Enter text ..."></textarea>
+       
+            <script type="text/javascript" charset="utf-8">
+                new nicEditor({ fullPanel: true, icononSave: function (content, id, instance) {
+                    alert('save button clicked for element ' + id + ' = ' + content);
+                } 
+                }).panelInstance('txtarea');</script>
+    </div>
+</asp:Content>
+
---
+++ FanataficsFinal/Site/NewStory.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessObjects;
+
+namespace Site
+{
+    public partial class NewStory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ddlCategory_Populate();
+                ddlGenre_Populate();
+                ddlMaturity_Populate();
+
+            }
+
+            else
+            {
+                ddlFandom_Populate();
+                
+            }
+
+            
+        }
+
+        void ddlCategory_Populate()
+        {
+            CategoryList catList = new CategoryList();
+            catList = catList.GetAll();
+            ddlCategory.DataSource = catList.List;
+            ddlCategory.DataTextField = "Type";
+            ddlCategory.DataValueField = "ID";
+            ddlCategory.DataBind();
+        }
+
+        void ddlFandom_Populate()
+        {
+            if (ddlCategory.Items.Count > 0)
+            {
+                FandomList fandList = new FandomList();
+                fandList = fandList.GetByCategoryID(new Guid(ddlCategory.SelectedValue));
+                ddlFandom.DataSource = fandList.List;
+                ddlFandom.DataBind();
+                               
+            }
+        }
+
+        void ddlGenre_Populate()
+        {
+            GenreList genList = new GenreList();
+            genList.GetAll();
+            ddlGenre.DataSource = genList.List;
+            ddlGenre.DataTextField = "GenreType";
+            ddlGenre.DataValueField = "ID";
+            ddlGenre.DataBind();
+        }
+
+        void ddlMaturity_Populate()
+        {
+            MaturityList matList = new MaturityList();
+            matList.GetAll();
+            ddlMaturity.DataSource = matList.List;
+            ddlMaturity.DataTextField = "MaturityLevel";
+            ddlMaturity.DataValueField = "ID";
+            ddlMaturity.DataBind();
+        }
+
+        void Add_Story()
+        {
+            Story story = new Story();
+            story.Title = this.txtTitle.Value;
+            story.Summary = this.txtSummary.Value;
+            story.MaturityID = new Guid(this.ddlMaturity.SelectedValue);
+
+            if (story.IsSavable() == true)
+            {
+                story = story.Save();
+            }
+        }
+
+
+        protected void btnAddStory_Click(object sender, EventArgs e)
+        {
+            Add_Story();
+        }
+
+        
+    }
+}
